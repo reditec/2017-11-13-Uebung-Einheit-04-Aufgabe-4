b0ba6ce281dfe142ec83c568547acadc0917e5d0
reditec <reditec@outlook.com>
2017-11-13 18:21:16 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 10;
+
+        while (x > 5) {
+            // Aufgabe 1
+            /* nichts */
+
+            // Aufgabe 2
+            /* y--; */
+
+            // Aufgabe 3
+            /* y = 20 - x; */
+
+            // Aufgabe 4
+            /* y *= 2; */
+
+            // Aufgabe 5
+            /* x -= 2; */
+
+            // Aufgabe 6
+            /* x -= 4;
+            y = 19; */
+
+            System.out.print(y + " ");
+            x --;
+        }
+    }
+
+}
